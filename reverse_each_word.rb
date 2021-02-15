1f50160dfc1d34c5b8e92f5596561acaffb42ae6
Jouse then <jthen93@gmail.com>
2021-02-15 05:51:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
   arr = sentence.split(" ")
   reversed_arr = []
   
-  arr.each do |word|
-    reversed_arr << word
+  arr.collect do |word|
+    reversed_arr << word.reverse
   end
   
   return reversed_arr
